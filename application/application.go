4dc5d7b1219bb7f471939685dff8b9e7285bab6d
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-13 22:09:04 +0330

    application and config packages

---
+++ application/application.go
@@ -0,0 +1,11 @@
+package application
+
+import (
+	"Meow/config"
+	"log"
+)
+
+type Application struct {
+	Config config.Config
+	Logger *log.Logger
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,9 @@
 package main
 
+// Declare a string containing the application version number.
+// Version number hard-coded constant.
+const version = "1.0.0"
+
 func main() {
 
 }
