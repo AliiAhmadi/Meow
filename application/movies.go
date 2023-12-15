babe788e1bb7e6957d1ec28d71f1ad987df6d22b
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-15 14:19:37 +0330

    implement basic json decoding

--- application/movies.go
+++ application/movies.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"Meow/internal/data"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,7 +10,24 @@ import (
 
 // Add createMovieHandler for the "POST /v1/movies" endpoint.
 func (app *Application) createNewMovieHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(writer, "Creating new movie.")
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	err := json.NewDecoder(request.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(writer, request, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(writer, "%+v\n", input)
 }
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint.
