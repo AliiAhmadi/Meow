cea67523c382536c8e40ef5865531ca556372ef5
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-15 09:01:22 +0330

    hide movie struct fields

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // Unique integer ID for movies
-	CreatedAt time.Time // Timestamp for creation of a movie
-	Title     string    // String title for movie
-	Year      int32     // Movie release year
-	Runtime   int32     // Movie time in minutes
-	Genres    []string  // Slice of genres for the movie
-	Version   int32     // The version number starts at 1 and will be incremented each time the movie information is updated
+	ID        int64     `json:"id"`         // Unique integer ID for movies
+	CreatedAt time.Time `json:"created_at"` // Timestamp for creation of a movie
+	Title     string    `json:"title"`      // String title for movie
+	Year      int32     `json:"year"`       // Movie release year
+	Runtime   int32     `json:"runtime"`    // Movie time in minutes
+	Genres    []string  `json:"genres"`     // Slice of genres for the movie
+	Version   int32     `json:"version"`    // The version number starts at 1 and will be incremented each time the movie information is updated
 }
